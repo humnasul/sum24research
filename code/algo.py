7b73a51c90849e14c6a290d52f3d4502f3562f18
Humna <humnasultan@gmail.com>
2024-06-15 12:00:06 -0400

    added comments for eqs to work on

---
+++ code/algo.py
@@ -0,0 +1,18 @@
+import numpy as np
+# D = min [ alpha * D sub 0.99, Dmax]
+"""
+M Matrix (mask)
+
+"""
+# Laplacian based on restricted gradients
+# Algebraic Eqs for Different Fourier Components
+# Fourier Transform Algo from C --> Python
+# Remove artifacts from striped image component - using nonlinear Gaussian Type Filter
+"""
+Filter parameter info
+Calculate...
+- delta r
+- sigma0, sigma0^2
+- sigma
+- value of filter parameter
+"""
