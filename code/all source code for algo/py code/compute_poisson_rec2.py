e36911c1e782925257cfe8cf0a70904b958fc9e8
Humna <humnasultan@gmail.com>
2024-06-21 13:39:51 -0400

    poisson calcs (2nd file)

---
+++ code/all source code for algo/py code/compute_poisson_rec2.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pyfftw
+
+def compute_poisson_rec(input_image, output_image, binary_M, grid_dct, sx, sy, Lap, Lap_dct, plan_forward, plan_backward):
+    
+    
+    return
