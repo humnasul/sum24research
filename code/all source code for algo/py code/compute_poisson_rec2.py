03f3ac3b3edf14cd6eb40c548588766116b08865
Humna <humnasultan@gmail.com>
2024-06-24 18:18:07 -0400

    Update compute_poisson_rec2.py

--- code/all source code for algo/py code/compute_poisson_rec2.py
+++ code/all source code for algo/py code/compute_poisson_rec2.py
@@ -38,14 +38,32 @@ def compute_poisson_rec(input_image, output_image, binary_M, grid_dct, sx, sy, L
     Lap[sy-1, 0] = -input_image[sy-1, 0] + input_image[sy-1, 1]
     if binary_M[sy-2, 0] != 0:
         Lap[sy-1, 0] += input_image[sy-2, 0] - input_image[sy-1, 0]
+    
+    for ix in range (1, sx-1):
+          Lap[sy-1][ix] = input_image[sy-1][ix-1] - 2.0*input_image[sy-1][ix] + input_image[sy-1][ix+1]
+          if binary_M[sy-2, ix] != 0:
+                Lap[sy-1, ix] += input_image[sy-2, ix] - input_image[sy-1, ix]
 
-    for(ix=1; ix<(sx-1); ix++){
-        Lap[sy-1][ix] = input_image[sy-1][ix-1] - 2.0*input_image[sy-1][ix] + input_image[sy-1][ix+1];
-        if(binary_M[sy-2][ix]!=0) Lap[sy-1][ix] += input_image[sy-2][ix] - input_image[sy-1][ix];
-    }
-
-    Lap[sy-1][sx-1] = input_image[sy-1][sx-2] - input_image[sy-1][sx-1];
-    if(binary_M[sy-2][sx-1]!=0) Lap[sy-1][sx-1] += input_image[sy-2][sx-1] - input_image[sy-1][sx-1];
+    Lap[sy-1, sx-1] = input_image[sy-1, sx-2] - input_image[sy-1, sx-1]
+    if binary_M[sy-2, sx-1] != 0:
+          Lap[sy-1, sx-1] += input_image[sy-2, sx-1] - input_image[sy-1, sx-1]
         
+    '''
+        // now we need to solve Discrete Poisson Equation  [Eq. 4 in ref. 2] 
+        // use FFT to do a forward DCT 
+        fftwf_execute_r2r(plan_forward, Lap[0], Lap_dct[0]);
+
+        // solve discrete Poisson equation (algabraic equations in Fourier space, Eq. 5 in ref. 2)
+        // by scaling Fourier components
+        #pragma omp parallel for
+            for(ix=0; ix<sx*sy; ix++) { Lap_dct[0][ix] *= grid_dct[0][ix]; }
+
+            // use FFT to do a backward DCT, and get the solution 
+            fftwf_execute_r2r(plan_backward, Lap_dct[0], Lap[0]);
+
+            // copy data to output array 
+        #pragma omp parallel for
+            for(ix=0; ix<sx*sy; ix++) { output_image[0][ix] = Lap[0][ix]; }
+    '''
     return
 
