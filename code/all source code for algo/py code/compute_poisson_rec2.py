f6ebd7040cea7a0865ae26e376b26b96789425bd
Humna <humnasultan@gmail.com>
2024-06-24 17:42:25 -0400

    Update compute_poisson_rec2.py

--- code/all source code for algo/py code/compute_poisson_rec2.py
+++ code/all source code for algo/py code/compute_poisson_rec2.py
@@ -7,9 +7,13 @@ def compute_poisson_rec(input_image, output_image, binary_M, grid_dct, sx, sy, L
     if binary_M[0, 0] != 0:
                 Lap[0, 0] += input_image[1, 0] - input_image[0, 0]
     for ix in range(1, sx - 1):
-        Lap[0, ix] = input_image[0, ix-1] - 2.0*input_image[0, ix] + input_image[0, ix+1];
+        Lap[0, ix] = input_image[0, ix-1] - 2.0*input_image[0, ix] + input_image[0, ix+1]
         if binary_M[0, ix]!=0: 
               Lap[0, ix] += input_image[1, ix] - input_image[0, ix]
     
+    Lap[0, sx-1] = input_image[0, sx-2] - input_image[0, sx-1]
+    if binary_M[0, sx-1]!=0: 
+              Lap[0, sx-1] += input_image[1, sx-1] - input_image[0, sx-1]
+
     return
 
